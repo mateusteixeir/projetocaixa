a6888f1178fbc7973e89e358d80576e9343c4550
unknown <mateusteixeiradas@gmail.com>
2024-11-20 14:34:11 -0300

    integracao front com api

---
+++ api-cachcontrol/src/main/java/cashcontrol/mat/api/api/CorsConfiguration.java
@@ -0,0 +1,13 @@
+package cashcontrol.mat.api.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200","null").allowedMethods("GET","POST","PUT","DELETE","HEAD","TRACE","OPTIONS","CONNECT");
+    }
+}
